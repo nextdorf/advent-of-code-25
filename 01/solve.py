9108f78b97dcf29167963f6a39653513bb5752b6
nextdorf <nextdorf@users.noreply.github.com>
2025-12-01 21:52:18 +0100

    Day 1 part 2

--- 01/solve.py
+++ 01/solve.py
@@ -4,9 +4,27 @@ import numpy as np
 from pathlib import Path
 
 with open(Path(__file__).parent / 'secret.input') as f:
-  nums = [50] + [(dict(R=1,L=-1)[si[0]] * int(si[1:])) % 100 for si in f.readlines() if si]
+  num0 = 50
+  nums = [(dict(R=1,L=-1)[si[0]] * int(si[1:])) for si in f.readlines() if si]
 
-cum_nums = np.cumsum(nums) % 100
-password = (cum_nums == 0).sum().item()
-print(password)
+def part_one():
+  cum_nums = np.cumsum([num0] + nums) % 100
+  password = (cum_nums == 0).sum().item()
+  return password
+
+def part_two():
+  clicks = int(num0 == 0)
+  x = num0
+  for n in nums:
+    if n >=0:
+      click_inc = (x + n) // 100
+    else:
+      click_inc = (100 - (x + n)) // 100 - int(x==0)
+    clicks += click_inc
+    x = (x + n) % 100
+  return clicks
+
+
+print('Part 1:', part_one())
+print('Part 2:', part_two())
 
