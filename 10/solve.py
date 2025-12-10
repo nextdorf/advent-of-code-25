21281a240b4a16a5617e3ccd9a21bcbc284965f7
nextdorf <nextdorf@users.noreply.github.com>
2025-12-10 07:28:46 +0100

    Stuck at day 10 part 2

--- 10/solve.py
+++ 10/solve.py
@@ -16,16 +16,46 @@ def iter_powerset(xs):
   for i in range(len(xs) + 1):
     yield from itertools.combinations(xs, i)
 
-min_counts = []
-for l, bts in zip(lights, buttons):
-  for bs in iter_powerset(bts):
-    x = l
-    for bt in bs:
-      x ^= bt
-    if not x:
-      min_counts.append(len(bs))
+def binary_to_vec(x, min_len=0):
+  res = []
+  for i in (2**j for j in range(64)):
+    if i > x:
       break
-min_count = sum(min_counts)
+    res.append(bool(x & i))
+  res = np.concat([res, [0]*(min_len-len(res))]).astype(np.int_)
+  return res
 
 
-print(min_count)
+def part_one():
+  min_counts = []
+  for l, bts in zip(lights, buttons):
+    for bs in iter_powerset(bts):
+      x = l
+      for bt in bs:
+        x ^= bt
+      if not x:
+        min_counts.append(len(bs))
+        break
+  min_count = sum(min_counts)
+  return min_count
+
+def part_two():
+  min_counts = []
+  for l, bts, jol in zip(lights, buttons, map(np.array, joltage)):
+    c = np.inf
+    for bs in iter_powerset(bts):
+      x = l
+      for bt in bs:
+        x ^= bt
+      if not x:
+        x_vec = binary_to_vec(x)
+        jol0 = np.sum([binary_to_vec(b, len(x_vec)) for b in bs], axis=0)
+        jol1 = jol
+        # TODO: find way of pressing zeros and reaching the left over joltage
+
+    min_counts.append(c)
+  min_count = sum(min_counts)
+  return min_count
+
+
+print('Part 1:', part_one())
